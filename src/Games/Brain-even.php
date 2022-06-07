3295d11ab6948059b7e9e4bd811001e66506e73c
prozet <abcd-xyz@list.ru>
2022-06-07 10:47:57 -0700

    st 6 done

--- src/Games/Brain-even.php
+++ src/Games/Brain-even.php
@@ -5,7 +5,6 @@ namespace BrainGames\BrainEven;
 function makeQuestionAndAnswer()
 {
     return function () {
-        
         $res = [rand(0, 99)];
         $res[] = $res[0] % 2 === 0 ? "yes" : "no";
         return $res;
